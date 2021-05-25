d732f08d497ea7eb6e6da8ff72fa103809c3f665
Erik <erikmat@berkeley.edu>
2021-05-25 08:45:26 -0700

    testing git ignore

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
-import express, {Application, Request, Response, NextFunction} from "express";
+import express, { Application, Request, Response, NextFunction } from 'express';
 //the extra variables available from express define express types
-import path from "path";
-import dotenv from 'dotenv'
-import fetch from 'node-fetch'
-import redis from 'redis'
+import path from 'path';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import redis from 'redis';
 //if curious see package.json, had to install types for some of these dependencies to work with TS
 
 dotenv.config();
@@ -12,27 +12,20 @@ dotenv.config();
 //const PORT = 3000
 //const PORT: number = parseInt(process.env.PORT as string, 3000);
 const PORT = process.env.PORT || 3000;
-const REDIS_PORT:any = process.env.REDIS_PORT;
+const REDIS_PORT: any = process.env.REDIS_PORT;
 //const REDIS_PORT: number = parseInt(process.env.REDIS_PORT as string, 6379);
 //console.log(REDIS_PORT)
-const client = redis.createClient(parseInt(REDIS_PORT))
-
-
+const client = redis.createClient(parseInt(REDIS_PORT));
 
 const app: Application = express();
-app.use(express.json())
-
-
+app.use(express.json());
 
 // app.use(express.static(path.join(__dirname, '../client')));
 
-app.get('/', (req: Request, res: Response)=>{
-    return res.status(200).sendFile(path.join(__dirname, './views/index.html'));
-})
+app.get('/', (req: Request, res: Response) => {
+  return res.status(200).sendFile(path.join(__dirname, './views/index.html'));
+});
 
-
-
-
-app.listen( PORT, () => {
-    console.log( `server started at http://localhost:${ PORT }` );
-} );
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
